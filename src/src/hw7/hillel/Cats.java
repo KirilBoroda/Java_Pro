1da67e20e9a08273639e2bee912e4486b585fcbe
Kyryl Boroda <kirilboroda25@gmail.com>
2023-03-08 20:19:29 +0200

    error correction

--- src/src/hw7/hillel/Cats.java
+++ src/src/hw7/hillel/Cats.java
@@ -3,16 +3,15 @@ package hw7.hillel;
 public class Cats extends Animals {
     @Override
     public void swim(int swiming) {
-        System.out.println("Cat cant swim!!!");
+        System.out.println(getName() + " cant swim!!!");
     }
 
     @Override
     public void run(int runnig) {
         if (runnig > 200) {
-            System.out.println("Cat cant run more than 200 m");
-        }
-        else {
-            System.out.println("Cat run:" + runnig);
+            System.out.println(getName() + " cant run more than 200 m");
+        } else {
+            System.out.println(getName() + " run:" + runnig);
         }
     }
 
--- src/src/hw7/hillel/Dogs.java
+++ src/src/hw7/hillel/Dogs.java
@@ -4,20 +4,18 @@ public class Dogs extends Animals {
     @Override
     public void swim(int swiming) {
         if (swiming > 10) {
-            System.out.println("A dog cannot swim more than 10 meters");
-        }
-        else {
-            System.out.println("Dog swim:" + swiming);
+            System.out.println(getName() + " cannot swim more than 10 meters");
+        } else {
+            System.out.println(getName() + " swim:" + swiming);
         }
     }
 
     @Override
     public void run(int runnig) {
         if (runnig > 500) {
-            System.out.println("A dog cannot run more than 500 meters ");
-        }
-        else {
-            System.out.println("Dog run:" + runnig);
+            System.out.println(getName() + " cannot run more than 500 meters ");
+        } else {
+            System.out.println(getName() + " run:" + runnig);
         }
     }
 
