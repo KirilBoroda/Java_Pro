420dfedad0b24164cf3320e68cacfbc6095b43c0
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-02 16:15:52 +0300

    hw rework

--- src/src/fileLoger/hillel/FileLogger.java
+++ src/src/fileLoger/hillel/FileLogger.java
@@ -14,25 +14,31 @@ public class FileLogger {
     }
 
     public void debug(String message) {
-        if (config.getLevel().compareTo(LoggingLevel.DEBUG) >= 0) {
+        if (isDebugEnabled()) {
             log("DEBUG", message);
         }
     }
+    public boolean isDebugEnabled() {
+        return config.getLevel().compareTo(LoggingLevel.DEBUG) >= 0;
+    }
 
     public void info(String message) {
-        if (config.getLevel().compareTo(LoggingLevel.INFO) >= 0) {
+        if (isInfoEnabled()) {
             log("INFO", message);
         }
     }
-
+    public boolean isInfoEnabled() {
+        return config.getLevel().compareTo(LoggingLevel.INFO) >= 0;
+    }
 
     private void log(String level, String message) {
         File file = new File(config.getFile());
         long fileSize = file.length();
         long maxFileSize = config.getMaxFileSize();
-        if (fileSize >= maxFileSize) {
+        while (fileSize >= maxFileSize) {
             String fileName = generateNewFileName(config.getFile());
             file = new File(fileName);
+            fileSize = file.length();
         }
         String logEntry = "[" + getCurrentTime() + "][" + level + "] Message: " + message;
         try (FileWriter writer = new FileWriter(file, true)) {
@@ -44,7 +50,7 @@ public class FileLogger {
 
     private String generateNewFileName(String file) {
         String fileName1 = "log";
-        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy;MM;dd;HH;mm;ss"));
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd.HH.mm.ss"));// In Windows cant use symbols such as : | _-
         String format = ".txt";
         return fileName1 + date + format;
 
--- src/src/fileLoger/hillel/Main.java
+++ src/src/fileLoger/hillel/Main.java
@@ -9,6 +9,5 @@ public class Main {
         FileLogger logger = new FileLogger("C:\\work\\Java_Pro\\src\\src\\fileLoger\\hillel\\logging.properties");
             logger.debug("This is a debug message.");
             logger.info("This is an info message.");
-
     }
 }
