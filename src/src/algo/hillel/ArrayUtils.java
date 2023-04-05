ab11aab4803a08d6e22c738494d4af23ca207df0
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-05 14:31:08 +0300

    hw rework

--- src/src/algo/hillel/ArrayUtils.java
+++ src/src/algo/hillel/ArrayUtils.java
@@ -3,7 +3,7 @@ package algo.hillel;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ArrayUtils {
-    public static  int[]  doArray(int size){
+    public final static  int[]  doArray(int size){
         int[] ints =new int[size];
         for (int i=0;i<ints.length;i++){
             ints[i] = ThreadLocalRandom.current().nextInt(size);
