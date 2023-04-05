5ded4a4b16bf7f106798c2ab921295659ecdad75
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-05 15:43:01 +0300

    hw rework

--- src/src/algo/hillel/InsertionSort.java
+++ src/src/algo/hillel/InsertionSort.java
@@ -13,14 +13,13 @@ public class InsertionSort {
     private static int[] insertSort(int[] ints) {
         for (int i = 1; i < ints.length; i++) {
             int current = ints[i];
-            int j = i;
-            while (j > 0 && ints[j - 1] > current) {
-                ints[j] = ints[j - 1];
+            int j = i - 1;
+            while (j >= 0 && ints[j] > current) {
+                ints[j + 1] = ints[j];
                 j--;
             }
-            ints[j] = current;
+            ints[j + 1] = current;
         }
         return ints;
     }
-
 }
