dfc61c25fa5895b0884b94bfed7e3394a38060c2
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-20 19:21:44 +0300

    hw18

--- src/src/fruit/hillel/Main.java
+++ src/src/fruit/hillel/Main.java
@@ -5,26 +5,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Box gg = new Box();
-        List<Fruit> gf = new ArrayList<>();
-
-        gf.add(new Apple());
-        gf.add(new Apple());
-        gf.add(new Apple());
-        gf.add(new Apple());
-        gf.add(new Apple());
-        gg.addFruits(gf);
-        Box gr = new Box();
-        List<Fruit> gr1 = new ArrayList<>();
-        gr1.add(new Apple());
-        gr1.add(new Apple());
-        gr1.add(new Apple());
-        gr1.add(new Apple());
-        gr1.add(new Apple());
-        gr.addFruits(gr1);
-        System.out.println(gg.getWeight());
-        gr.merge(gg);
-        System.out.println(gr.getWeight());
-        System.out.println(gg.getWeight());
+        Box box1 = new Box();
+        List<Fruit> listFruits1 = new ArrayList<>();
+        listFruits1.add(new Apple());
+        listFruits1.add(new Apple());
+        listFruits1.add(new Apple());
+        listFruits1.add(new Apple());
+        listFruits1.add(new Apple());
+        box1.addFruits(listFruits1);
+        Box box2 = new Box();
+        List<Fruit> listFruits2 = new ArrayList<>();
+        listFruits2.add(new Apple());
+        listFruits2.add(new Apple());
+        listFruits2.add(new Apple());
+        listFruits2.add(new Apple());
+        listFruits2.add(new Apple());
+        box2.addFruits(listFruits2);
+        System.out.println(box1.getWeight());
+        box2.merge(box1);
+        System.out.println(box2.getWeight());
+        System.out.println(box1.getWeight());
     }
 }
