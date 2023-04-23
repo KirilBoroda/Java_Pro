1e4732bf7cd3b0c026b54ef8400ac9dc819b220d
Kyryl Boroda <kirilboroda25@gmail.com>
2023-04-23 23:57:15 +0300

    hw17

--- src/src/coffee/order/Order.java
+++ src/src/coffee/order/Order.java
@@ -1,5 +1,7 @@
 package coffee.order;
 
+import java.util.Objects;
+
 public class Order {
     private int orderNumber;
     private String customerName;
@@ -15,5 +17,23 @@ public class Order {
 
     public String getCustomerName() {
         return customerName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+
+        if (orderNumber != order.orderNumber) return false;
+        return Objects.equals(customerName, order.customerName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = orderNumber;
+        result = 31 * result + (customerName != null ? customerName.hashCode() : 0);
+        return result;
     }
 }
